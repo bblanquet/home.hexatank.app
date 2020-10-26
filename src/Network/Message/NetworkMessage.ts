9bbe3f0ebe27cd64d6e086b4490d022c7529e015
unknown <blanquet.bertrand@gmail.com>
2020-10-26 21:48:59 +0100

    start tcp

--- src/Network/Message/NetworkMessage.ts
+++ src/Network/Message/NetworkMessage.ts
@@ -1,12 +1,16 @@
+import { ProtocolKind } from './ProtocolKind';
 import { isNullOrUndefined } from '../../Core/Utils/ToolBox';
 import { INetworkMessage } from './INetworkMessage';
 import { PacketKind } from './PacketKind';
 
 export class NetworkMessage<T> implements INetworkMessage {
 	public Content: T;
-	public Emitter: string;
 	public RoomName: string;
+
+	public Emitter: string;
 	public Recipient: string;
+
+	public Protocol: ProtocolKind;
 	public Kind: PacketKind;
 	public EmittedDate: number;
 	public Latency: number | null = null;

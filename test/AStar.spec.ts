4aa3f2b1c8ee7876ea506ae710b17c07fdf0301d
bertrand Blanquet <blanquet.bertrand@gmail.com>
2021-01-06 23:06:17 +0100

    tests

--- test/AStar.spec.ts
+++ test/AStar.spec.ts
@@ -1,45 +1,37 @@
+import { Dictionnary } from './../src/Core/Utils/Collections/Dictionnary';
 import { GameSettings } from './../src/Core/Framework/GameSettings';
 import { CircleMapBuilder } from './../src/Core/Setup/Builder/CircleMapBuilder';
 import { AStarEngine } from './../src/Core/Ia/AStarEngine';
-import { CellContext } from './../src/Core/Items/Cell/CellContext';
 import 'mocha';
 import { expect } from 'chai';
 import { HexAxial } from '../src/Core/Utils/Geometry/HexAxial';
-import { SimpleCell } from './SimpleCell';
 
 describe('Astar', () => {
-	var cells = new CellContext<SimpleCell>();
+	var cells = new Dictionnary<HexAxial>();
 
 	before(() => {
 		GameSettings.MapSize = 6;
 		var playgroundMaker = new CircleMapBuilder();
-		playgroundMaker.Build(6).forEach((coo) => {
-			cells.Add(new SimpleCell(coo, cells));
+		playgroundMaker.GetAllCoos(10).forEach((coo) => {
+			cells.Add(coo.ToString(), coo);
 		});
 	});
 	it('Find path 44 to 44', () => {
-		var startcell = cells.Get(new HexAxial(4, 4));
-		var goalcell = cells.Get(new HexAxial(4, 4));
-		var result = new AStarEngine<SimpleCell>((e) => true, (e) => 1).GetPath(startcell, goalcell);
+		var startcell = cells.Get(new HexAxial(4, 4).ToString());
+		var goalcell = cells.Get(new HexAxial(4, 4).ToString());
+		var result = new AStarEngine<HexAxial>((e) => true, (e) => 1).GetPath(startcell, goalcell);
 		expect(0).to.equals(result.length);
 	});
 	it('Find path 44 to 54', () => {
-		var startcell = cells.Get(new HexAxial(4, 4));
-		var goalcell = cells.Get(new HexAxial(5, 4));
-		var result = new AStarEngine<SimpleCell>((e) => true, (e) => 1).GetPath(startcell, goalcell);
+		var startcell = cells.Get(new HexAxial(4, 4).ToString());
+		var goalcell = cells.Get(new HexAxial(5, 4).ToString());
+		var result = new AStarEngine<HexAxial>((e) => true, (e) => 1).GetPath(startcell, goalcell);
 		expect(1).to.equals(result.length);
 	}).timeout(500);
 	it('Find path 44 to 64', () => {
-		var startcell = cells.Get(new HexAxial(4, 4));
-		var goalcell = cells.Get(new HexAxial(6, 4));
-		var result = new AStarEngine<SimpleCell>((e) => true, (e) => 1).GetPath(startcell, goalcell);
+		var startcell = cells.Get(new HexAxial(4, 4).ToString());
+		var goalcell = cells.Get(new HexAxial(6, 4).ToString());
+		var result = new AStarEngine<HexAxial>((e) => true, (e) => 1).GetPath(startcell, goalcell);
 		expect(2).to.equals(result.length);
-	}).timeout(500);
-	it('Find path 33 to 67', () => {
-		var startcell = cells.Get(new HexAxial(3, 3));
-		var goalcell = cells.Get(new HexAxial(6, 4));
-		var c = cells;
-		var result = new AStarEngine<SimpleCell>((e) => true, (e) => 1).GetPath(startcell, goalcell);
-		expect(4).to.equals(result.length);
 	}).timeout(500);
 });
--- test/HexAxial.spec.ts
+++ test/HexAxial.spec.ts
@@ -7,12 +7,12 @@ describe('HexAxial', () => {
 		var coordinate = new HexAxial(4, 4);
 		var neighbours = coordinate.GetNeighbours();
 		expect(neighbours.length).to.equal(6);
-		expect(neighbours[0]).to.contains(new HexAxial(3, 4));
-		expect(neighbours[1]).to.contains(new HexAxial(3, 5));
-		expect(neighbours[2]).to.contains(new HexAxial(4, 3));
-		expect(neighbours[3]).to.contains(new HexAxial(4, 5));
-		expect(neighbours[4]).to.contains(new HexAxial(5, 3));
-		expect(neighbours[5]).to.contains(new HexAxial(5, 4));
+		expect(neighbours[0]).to.contains(new HexAxial(3, 5));
+		expect(neighbours[1]).to.contains(new HexAxial(3, 4));
+		expect(neighbours[2]).to.contains(new HexAxial(4, 5));
+		expect(neighbours[3]).to.contains(new HexAxial(4, 3));
+		expect(neighbours[4]).to.contains(new HexAxial(5, 4));
+		expect(neighbours[5]).to.contains(new HexAxial(5, 3));
 	});
 
 	it('it should get all neighbours range 2', () => {

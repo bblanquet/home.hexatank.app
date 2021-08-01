0fc8e3ad0bdeecf31a446e08b43bb945d3f0ad0c
bertrand Blanquet <bertrand.blanquet6@gmail.com>
2021-08-01 19:36:09 +0900

    get version loading page

--- src/Components/Screens/LoadingScreen.tsx
+++ src/Components/Screens/LoadingScreen.tsx
@@ -2,12 +2,12 @@ import { h, Component } from 'preact';
 import Btn from '../Common/Button/Stylish/Btn';
 import { ColorKind } from '../Common/Button/Stylish/ColorKind';
 import Icon from '../Common/Icon/IconComponent';
-import Background from '../Components/Background';
 import Switch from '../Common/Struct/Switch';
 import { LoadingState } from '../Model/LoadingState';
 import { HookedComponent } from '../Hooks/HookedComponent';
 import { LoadingHook } from '../Hooks/LoadingHook';
 import { useState } from 'preact/hooks';
+import Body from '../Common/Struct/Body';
 export default class LoadingScreen extends HookedComponent<{}, LoadingHook, LoadingState> {
 	public GetDefaultHook(): LoadingHook {
 		return new LoadingHook(useState(LoadingHook.DefaultState()));
@@ -15,37 +15,45 @@ export default class LoadingScreen extends HookedComponent<{}, LoadingHook, Load
 
 	public Rendering(): h.JSX.Element {
 		return (
-			<Background>
-				<div class="sizeContainer absolute-center-middle-2">
-					<div class="logo-container">
-						<div class="fill-logo-back-container">
-							<div class="fill-logo-back spin-fade" />
-						</div>
-						<div class="fill-tank-logo slow-bounce" />
-						<div class="fill-logo" />
-					</div>
-					<div class="container">
-						<div class="progress progress-striped">
-							<div class="progress-bar" style={`width:${this.Hook.State.Percentage}%`} />
-						</div>
-					</div>
-					<Switch
-						isLeft={this.Hook.State.IsLoading}
-						left={
-							<div class="container-center" style="color:white;font-weight:bold;text-align:center;">
-								{this.Hook.GetSentence()}
+			<Body
+				header={null}
+				content={
+					<div class="sizeContainer absolute-center-middle-2">
+						<div class="logo-container">
+							<div class="fill-logo-back-container">
+								<div class="fill-logo-back spin-fade" />
 							</div>
-						}
-						right={
-							<div class="container-center">
-								<Btn OnClick={() => this.Hook.OnStart()} Color={ColorKind.Red}>
-									<Icon Value="fas fa-dungeon" /> Continue
-								</Btn>
+							<div class="fill-tank-logo slow-bounce" />
+							<div class="fill-logo" />
+						</div>
+						<div class="container">
+							<div class="progress progress-striped">
+								<div class="progress-bar" style={`width:${this.Hook.State.Percentage}%`} />
 							</div>
-						}
-					/>
-				</div>
-			</Background>
+						</div>
+						<Switch
+							isLeft={this.Hook.State.IsLoading}
+							left={
+								<div class="container-center" style="color:white;font-weight:bold;text-align:center;">
+									{this.Hook.GetSentence()}
+								</div>
+							}
+							right={
+								<div class="container-center">
+									<Btn OnClick={() => this.Hook.OnStart()} Color={ColorKind.Red}>
+										<Icon Value="fas fa-dungeon" /> Continue
+									</Btn>
+								</div>
+							}
+						/>
+					</div>
+				}
+				footer={
+					<div class="navbar nav-inner" style="font-weight:bold;">
+						<div>v 0.8.14</div>
+					</div>
+				}
+			/>
 		);
 	}
 }
